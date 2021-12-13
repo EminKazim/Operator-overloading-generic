86bb7470e95392fd1feadefd4c62233e4d058712
Emin Kazım <eminjk@code.edu.az>
2021-12-13 06:16:23 -0800

    Sort by age and generics

---
+++ Operator overloading, generic/Operator overloading, generic/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operator_overloading__generic
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Person(string name, string surname, int age)
+        {
+            Age = age;
+            Name = name;
+            Surname = surname;
+
+        }
+        public static bool operator >(Person person1, Person person2)
+        {
+            return person1.Age > person2.Age;
+        }
+
+        public static bool operator <(Person person1, Person person2)
+        {
+            return person1.Age < person2.Age;
+        }
+    }
+}
---
+++ Operator overloading, generic/Operator overloading, generic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Operator_overloading__generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayItems<Person> people = new ArrayItems<Person>();
+            people.Add(new Person("Hezret","Niftelizade", 25));
+            people.Add(new Person("Reshad", "Qasimzade", 11));
+            Person person2 = new Person("Sadiq", "Abdullayev", 9);
+            people.Add(person2);
+            Person person1 = new Person("Filankes", "Filankesov", 33);
+            
+            people.GetAll();
+
+            Console.WriteLine(person1>person2);
+        }
+    }
+}
